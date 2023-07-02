15b320647f7498f07ed71de9c60d2682f809efbe
David Cramer <David@erraticsoft.com>
2023-07-01 19:39:29 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+/*
+*
+*       Game Development with C++
+*       (C) David Cramer 
+*
+*     This is a project which will follow the
+*     book Beginning C++ through Game Development.
+*
+*
+*
+*
+*
+*
+*
+*
+*/
+
+
+
+
+#include<iostream>
+
+int main() {
+
+  std::cout << "Game Over!" << std::endl;
+return 0;
+}
